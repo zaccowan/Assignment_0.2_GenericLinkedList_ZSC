3bb717f21bd9dc488ede047a841d39b68b9c992a
zaccowan <68406370+zaccowan@users.noreply.github.com>
2022-09-02 17:04:15 -0400

    Added Javadocs

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"linkedList","l":"Application"},{"p":"linkedList","l":"LinkedList"}];updateSearchResults();
--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -1,7 +1,8 @@
 package linkedList;
 /**
- * @author njohnson3
- *
+ * Generic form of LinkedList class
+ * @author njohnson3, Zachary Cowan
+ * @version 9/1/2022
  */
 public class LinkedList<T> {
 	
@@ -14,15 +15,31 @@ public class LinkedList<T> {
 		length=0;
 		tail=head;
 	}
+	
+	/**
+	* Check if linked list is empty
+	* @return True if linked list is empty
+	*/
 	public boolean isEmpty()
 	{
 		if (head==null) { return true;}
 		else return false;
 	}
+	
+	/**
+	* Gets the head of the list
+	* @return The head node of linked list
+	*/
 	public Node<T> getList()
 	{
 		return head;
 	}
+	
+	/**
+	* Gets the head of the list
+	* @param aNode Node to add to list
+	* @return The head node of linked list
+	*/
 	public void addNode(Node<T> aNode)
 	{
 		if (isEmpty() ) {
@@ -34,6 +51,5 @@ public class LinkedList<T> {
 		tail=tail.nextNode;
 		tail.nextNode=null;
 		length++;
-		
 	}
 }
